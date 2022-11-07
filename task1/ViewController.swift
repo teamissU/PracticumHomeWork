2e38a96f32900d0831f651375f1615f5ff1f7caf
teamissU <morgunov295@gmail.com>
2022-11-07 16:33:42 +0300

    Initial Commit

---
+++ task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by Tim on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
