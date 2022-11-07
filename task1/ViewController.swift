b51eb745e23f9aff1e454c7c2ed6dd0fb1a98347
teamissU <morgunov295@gmail.com>
2022-11-07 20:33:02 +0300

    Сделано приложение со счетчиком

--- task1/ViewController.swift
+++ task1/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: Int = 0
 
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var score: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func increasingNumber(_ sender: Any) {
+        number += 1
+        score.text = "Значение счётчика: \(number)"
+    }
 }
 
